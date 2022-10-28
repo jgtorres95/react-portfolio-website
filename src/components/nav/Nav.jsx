f08597e44eebba59e851c95dbbe327b899131d9e
jgtorres95 <78467454+jgtorres95@users.noreply.github.com>
2022-10-27 21:04:06 -0700

    incorporate useState hook to manage activeNav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -4,12 +4,18 @@ import {AiOutlineHome} from 'react-icons/ai'
 import {AiOutlineUser} from 'react-icons/ai'
 import {BiBook} from 'react-icons/bi'
 import {BiMessageSquareDetail} from 'react-icons/bi'
+import {useState} from 'react'
 
 const Nav = () => {
+  // Incorporate useState hook to manage activeNav state. 'active' class is applied to nav links based on activeNav state value. 
+  const [activeNav, setActiveNav] = useState('#')
   return (
-    <div>
-      
-    </div>
+    <nav>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome/></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser/></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBook/></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail/></a>
+    </nav>
   )
 }
 
