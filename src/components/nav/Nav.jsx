0cafc3d479eb69a3fdd4332dc4d6054b3d13378c
jgtorres95 <78467454+jgtorres95@users.noreply.github.com>
2022-10-27 21:03:34 -0700

    import react icons

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import './nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {BiBook} from 'react-icons/bi'
+import {BiMessageSquareDetail} from 'react-icons/bi'
 
 const Nav = () => {
   return (
