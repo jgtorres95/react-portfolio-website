85b636ec2a562bd97be9b64ff43c8bafe7456c8c
jgtorres95 <78467454+jgtorres95@users.noreply.github.com>
2022-11-01 14:35:02 -0700

    create portfolio item

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -13,6 +13,22 @@ const Portfolio = () => {
     <section id="portfolio">
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__item-image"></div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com" className="btn" target="_blank">
+            Github
+          </a>
+          <a
+            href="https://google.com"
+            className="btn btn-primary"
+            target="_blank"
+          >
+            Live Demo
+          </a>
+        </article>
+      </div>
     </section>
   );
 };
