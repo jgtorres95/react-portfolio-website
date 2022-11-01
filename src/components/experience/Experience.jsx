06fb6208c1454a27732cb762fed259189f80645e
jgtorres95 <78467454+jgtorres95@users.noreply.github.com>
2022-11-01 14:11:23 -0700

    add import statements

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import './experience.css'
+import React from "react";
+import "./experience.css";
+import { BsPatchCheckFill } from "react-icons/bs";
 
 const Experience = () => {
   return (
