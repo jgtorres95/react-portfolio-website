4f3568e453dae2d8d2b20964ce20604a5f571cf4
jgtorres95 <78467454+jgtorres95@users.noreply.github.com>
2022-11-01 14:40:57 -0700

    create remaining portfolio items

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -15,7 +15,89 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
       <div className="container portfolio__container">
         <article className="portfolio__item">
-          <div className="portfolio__item-image"></div>
+          <div className="portfolio__item-image">
+            <img src="{IMG1}" alt="myFlix" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com" className="btn" target="_blank">
+            Github
+          </a>
+          <a
+            href="https://google.com"
+            className="btn btn-primary"
+            target="_blank"
+          >
+            Live Demo
+          </a>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src="{IMG1}" alt="myFlix" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com" className="btn" target="_blank">
+            Github
+          </a>
+          <a
+            href="https://google.com"
+            className="btn btn-primary"
+            target="_blank"
+          >
+            Live Demo
+          </a>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src="{IMG1}" alt="myFlix" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com" className="btn" target="_blank">
+            Github
+          </a>
+          <a
+            href="https://google.com"
+            className="btn btn-primary"
+            target="_blank"
+          >
+            Live Demo
+          </a>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src="{IMG1}" alt="myFlix" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com" className="btn" target="_blank">
+            Github
+          </a>
+          <a
+            href="https://google.com"
+            className="btn btn-primary"
+            target="_blank"
+          >
+            Live Demo
+          </a>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src="{IMG1}" alt="myFlix" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com" className="btn" target="_blank">
+            Github
+          </a>
+          <a
+            href="https://google.com"
+            className="btn btn-primary"
+            target="_blank"
+          >
+            Live Demo
+          </a>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src="{IMG1}" alt="myFlix" />
+          </div>
           <h3>This is a portfolio item title</h3>
           <a href="https://github.com" className="btn" target="_blank">
             Github
