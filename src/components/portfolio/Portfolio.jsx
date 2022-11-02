5aae2c32ef1a7033e74a5b070d71fb20f3f72f40
jgtorres95 <78467454+jgtorres95@users.noreply.github.com>
2022-11-02 15:31:02 -0700

    replace individual article tags with a map method

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -66,102 +66,24 @@ const Portfolio = () => {
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
       <div className="container portfolio__container">
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="myFlix" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <a href="https://github.com" className="btn" target="_blank">
-            Github
-          </a>
-          <a
-            href="https://google.com"
-            className="btn btn-primary"
-            target="_blank"
-          >
-            Live Demo
-          </a>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG2} alt="myFlix" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <a href="https://github.com" className="btn" target="_blank">
-            Github
-          </a>
-          <a
-            href="https://google.com"
-            className="btn btn-primary"
-            target="_blank"
-          >
-            Live Demo
-          </a>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG3} alt="myFlix" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <a href="https://github.com" className="btn" target="_blank">
-            Github
-          </a>
-          <a
-            href="https://google.com"
-            className="btn btn-primary"
-            target="_blank"
-          >
-            Live Demo
-          </a>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG4} alt="myFlix" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <a href="https://github.com" className="btn" target="_blank">
-            Github
-          </a>
-          <a
-            href="https://google.com"
-            className="btn btn-primary"
-            target="_blank"
-          >
-            Live Demo
-          </a>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG5} alt="myFlix" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <a href="https://github.com" className="btn" target="_blank">
-            Github
-          </a>
-          <a
-            href="https://google.com"
-            className="btn btn-primary"
-            target="_blank"
-          >
-            Live Demo
-          </a>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG2} alt="myFlix" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <a href="https://github.com" className="btn" target="_blank">
-            Github
-          </a>
-          <a
-            href="https://google.com"
-            className="btn btn-primary"
-            target="_blank"
-          >
-            Live Demo
-          </a>
-        </article>
+        {data.map(({ id, image, title, github, demo }) => {
+          return (
+            <article key={id} className="portfolio__item">
+              <div className="portfolio__item-image">
+                <img src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              <div className="portfolio__item-cta">
+                <a href={github} className="btn" target="_blank">
+                  Github
+                </a>
+                <a href={demo} className="btn btn-primary" target="_blank">
+                  Live Demo
+                </a>
+              </div>
+            </article>
+          );
+        })}
       </div>
     </section>
   );
