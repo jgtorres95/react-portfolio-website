8e5e99b29b3260e2adbeafd47ef1eab71ef90d8c
jgtorres95 <78467454+jgtorres95@users.noreply.github.com>
2022-11-07 00:12:51 -0800

    add imports

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import "./contact.css";
+import { MdOutlineEmail } from "react-icons/md";
+import { RiMessengerLine } from "react-icons/ri";
+import { BsWhatsapp } from "react-icons/bs";
+import { useRef } from "react";
+import emailjs from "emailjs-com";
 
 const Contact = () => {
   return <section id="contact"></section>;
