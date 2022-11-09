2157badaaabdf36cbcd3fba0b1b642a6c0efc307
jgtorres95 <78467454+jgtorres95@users.noreply.github.com>
2022-11-09 12:49:08 -0800

    update frontend skills

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -36,8 +36,50 @@ const Experience = () => {
             <article className="experience__details">
               <BsPatchCheckFill className="experinece__details-icon" />
               <div>
-                <h4>HTML</h4>
+                <h4>React</h4>
                 <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experinece__details-icon" />
+              <div>
+                <h4>Bootstrap</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experinece__details-icon" />
+              <div>
+                <h4>React Redux</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experinece__details-icon" />
+              <div>
+                <h4>React Native</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experinece__details-icon" />
+              <div>
+                <h4>Angular</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experinece__details-icon" />
+              <div>
+                <h4>Typescript</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experinece__details-icon" />
+              <div>
+                <h4>jQuery</h4>
+                <small className="text-light">Intermediate</small>
               </div>
             </article>
           </div>
