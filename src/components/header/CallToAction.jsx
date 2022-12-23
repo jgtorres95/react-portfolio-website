53c1ffc44a7088e8138b3a67d5625c7573d5cf0d
jgtorres95 <78467454+jgtorres95@users.noreply.github.com>
2022-12-23 00:48:42 -0800

    Update CallToAction.jsx

--- src/components/header/CallToAction.jsx
+++ src/components/header/CallToAction.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import CV from '../../assets/cv.pdf'
+import React from "react";
+import CV from "../../assets/cv.pdf";
 
 const CallToAction = () => {
   return (
-    <div className='cta'>
-      <a href={CV} download className='btn'>Download CV</a>
-      <a href="#contact" className='btn btn-primary'>Let's talk</a>
+    <div className="cta">
+      <a href={CV} download className="btn">
+        Download CV
+      </a>
+      <a href="#contact" className="btn btn-primary">
+        Let's talk
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default CallToAction
+export default CallToAction;
