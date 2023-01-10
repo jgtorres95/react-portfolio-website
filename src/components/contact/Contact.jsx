8c18e7c39fcbef564d381180d55e215ba42bb3d7
jgtorres95 <78467454+jgtorres95@users.noreply.github.com>
2023-01-10 13:14:10 -0800

    add alert for message

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -22,6 +22,7 @@ const Contact = () => {
       .then(
         (result) => {
           console.log(result.text);
+          alert("Message sent");
         },
         (error) => {
           console.log(error.text);
